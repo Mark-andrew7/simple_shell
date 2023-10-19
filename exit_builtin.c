5f066eac66c7a2d77743e7fdc27a90575a7fec58
Mark-andrew7 <kamaumark19@gmail.com>
2023-10-19 00:37:13 -0700

    exit arguments

--- exit_builtin.c
+++ exit_builtin.c
@@ -7,7 +7,16 @@ void handle_exit_command(char **arguments)
 {
 if (strcmp(arguments[0], "exit") == 0)
 {
+if (arguments[1] != NULL)
+{
+int status = atoi(arguments[1]);
+exit(status);
+}
+else
+
+{
 free_tokens(arguments);
 exit(0);
+}
 }
 }
